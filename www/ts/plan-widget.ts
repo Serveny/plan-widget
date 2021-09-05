80868f7a8a69e67ac61ce1d196f49088a221ccab
Dylan Lewis <dylan_lewis93@web.de>
2021-09-05 23:51:07 +0200

    added scss, wip

--- www/ts/plan-widget.ts
+++ www/ts/plan-widget.ts
@@ -1,10 +1,10 @@
-import { DataTable } from './data-table';
+import { DataTable } from './data-table/data-table.class';
 
 export class PlanWidget {
   resourceTable: DataTable;
 
   constructor(containerEl: HTMLElement | null) {
     if (containerEl == null) throw '[PlanWidget] containerEl is null';
-    this.resourceTable = new DataTable(containerEl);
+    this.resourceTable = new DataTable().appendTo(containerEl);
   }
 }
