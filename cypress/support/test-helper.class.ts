cb287795c04b0719787e425eba046a7972ebcd45
Serveny <fusion53@web.de>
2021-10-16 12:34:37 +0200

    wip

--- cypress/support/test-helper.class.ts
+++ cypress/support/test-helper.class.ts
@@ -1,11 +1,12 @@
 export default class TestHelper {
-  static testDrag(el: HTMLElement, x: number, y: number): void {
+  static testDrag(el: HTMLElement, body: HTMLBodyElement, 
+    x: number, y: number): void {
     const coords = el.getBoundingClientRect()
     el.dispatchEvent(new MouseEvent('mousedown'))
-    el.dispatchEvent(new MouseEvent('mousemove', 
+    body.dispatchEvent(new MouseEvent('mousemove', 
     { clientX: coords.x, clientY: coords.y }))
-    el.dispatchEvent(new MouseEvent('mousemove', 
+    body.dispatchEvent(new MouseEvent('mousemove', 
       { clientX: coords.x + x, clientY: coords.y + y }))
-    el.dispatchEvent(new MouseEvent('mouseup'))
+      body.dispatchEvent(new MouseEvent('mouseup'))
   }
 }
