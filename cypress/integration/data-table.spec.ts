e62fb1bc8363a324c5f0ba8cda434faa619b14f8
Dylan Lewis <dylan_lewis93@web.de>
2021-11-01 14:34:39 +0100

    Added data table addRows()

--- cypress/integration/data-table.spec.ts
+++ cypress/integration/data-table.spec.ts
@@ -1,7 +1,10 @@
 import { DataTable } from '../../src/www/ts/classes/data-table/data-table.class'
+import { DemoData } from '../../src/www/ts/demo-data'
+import { HorizontalTextAlign } from '../../src/www/ts/enums/horizontal-text-align.enum'
+import TestLayout from '../../src/www/ts/test-data/table-layout.data'
 
 describe('testing data-table', () => {
-  const dataTable = new DataTable(null)
+  const dataTable = new DataTable(null), dd = new DemoData()
 
   before('visit test site', () => {
     cy.visit('integration-test.html')
@@ -9,7 +12,7 @@ describe('testing data-table', () => {
       dataTable.appendTo(bodyEl[0]))
   })
 
-  it('append to container', () =>
+  it('appends to container', () =>
     cy.get('.data-table').should('be.visible'))
   it('has head', () =>
     cy.get('.dt-head').should('be.visible'))
@@ -21,5 +24,24 @@ describe('testing data-table', () => {
     dataTable.setTitle(title)
     expect(dataTable.layout?.title).equal(title)
     cy.contains('.dt-head', title).should('be.visible')
+  })
+
+  it(`shows data as rows`, () => {
+    const rows = dd.Resources
+    dataTable.setLayout(TestLayout)
+    dataTable.addRows(rows)
+    rows.forEach((_, i) => 
+      cy.get(`.dt-row[style*="order: ${i};"]`).should('exist')
+      .then(el => TestLayout.columns?.forEach(col => {
+        const colEl = el.children(
+          `.dt-row-cell[style*="order: ${col.visibleIndex};"]`)
+        expect(colEl.length).eq(col.visible ? 1 : 0)
+        if (col.visible) {
+          expect(colEl.css('width')).eq(col.width)
+          expect(colEl.css('text-align')).eq(col.textAlign != null 
+            ? HorizontalTextAlign[col.textAlign].toLowerCase() 
+            : undefined) 
+        } 
+      })))
   })
 })
